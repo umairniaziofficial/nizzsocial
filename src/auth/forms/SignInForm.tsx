82806cd491e06cb64bf4c878d5df3c952a8fc8c6
umairniaziofficial <umairniaziofficial@gmail.com>
2024-07-16 01:13:16 +0500

    Signin Complete

--- src/auth/forms/SignInForm.tsx
+++ src/auth/forms/SignInForm.tsx
@@ -1,7 +1,145 @@
-const SignInForm = () => {
-  return (
-    <div>SignInForm</div>
-  )
-}
+import { useToast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Loader from "@/components/ui/shared/Loader";
+import { Link, useNavigate } from "react-router-dom";
+import { useSigninAccount } from "@/lib/react-query/querriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
-export default SignInForm
+const SignInValidationSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+const SignInForm = () => {
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const { toast } = useToast();
+  const navigate = useNavigate();
+
+  const { mutateAsync: signInAccount, isPending } = useSigninAccount();
+
+  const form = useForm<z.infer<typeof SignInValidationSchema>>({
+    resolver: zodResolver(SignInValidationSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof SignInValidationSchema>) {
+    try {
+      const session = await signInAccount({
+        email: values.email,
+        password: values.password,
+      });
+
+      if (!session) {
+        throw new Error("Login Failed");
+      }
+
+      const isLoggedIn = await checkAuthUser();
+      if (isLoggedIn) {
+        form.reset();
+        navigate("/");
+      } else {
+        throw new Error("Login Failed");
+      }
+
+      toast({ title: "Logged in successfully!" });
+    } catch (error) {
+      toast({ title:"An error occurred. Please try again." });
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col text-white">
+        <div className="flex items-center">
+          <img src="/src/assets/Logo.svg" alt="logo" className="w-8" />
+          <p className="text-2xl pl-3 font-semibold">NizzSocial</p>
+        </div>
+        <h1 className="h3-bold md:h2-bold pt-5 sm:pt-8">
+          Log in to your account
+        </h1>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          Welcome back! Please enter your details.
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    className="shad-input"
+                    placeholder="Enter your email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    className="shad-input"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="shad-button_primary">
+            {isPending || isUserLoading ? (
+              <div className="flex-center gap-2">
+                <Loader />
+              </div>
+            ) : (
+              "Sign In"
+            )}
+          </Button>
+          <p className="text-sm-regular text-light-2 text-center mt-2">
+            Don't have an account?{" "}
+            <Link
+              to="/sign-up"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SignInForm;
