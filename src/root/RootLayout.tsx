f8b9db52651b4b477b84b8cc79616d41da241ab2
umairniaziofficial <umairniaziofficial@gmail.com>
2024-07-17 00:11:17 +0500

    Bottom Bar Layout Complete

--- src/root/RootLayout.tsx
+++ src/root/RootLayout.tsx
@@ -14,7 +14,7 @@ const RootLayout = () => {
           <Topbar />
           <LeftSidebar />
 
-          <section className="flex flex-1 h-full">
+          <section className="flex flex-1 h-screen">
             <Outlet />
           </section>
 
